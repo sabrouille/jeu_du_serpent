247e4734c4e903e419cf903e6ec2acbb2cb32a44
Sarah Brouillet <sarah.brouillet@gmail.com>
2020-11-20 09:57:38 -0500

    Initialisation de la classe Serpent, détection des touches clavier et déplacement du serpent

--- js/script.js
+++ js/script.js
@@ -41,18 +41,63 @@ document.addEventListener("DOMContentLoaded", function(event) {
            console.log("Création du serpent");
 
            this.leJeu = _leJeu;
+
+           this.currentX = -1;
+           this.currentY = 0;
+
+           this.nextMoveX = 1;
+           this.nextMoveY = 0;
+
+           this.serpentLongueur = 1;
+           this.tblCarreSerpent = [];
+
+           this.vitesse = 250;
+           this.timing = setInterval(this.controleSerpent.bind(this), this.vitesse);
+
+           document.addEventListener("keydown", this.verifTouche.bind(this));   //bind force à prendre Serpent comme this au lieu de document
        }
 
-       verifTouche(evt){
+       verifTouche(_evt){
+           var evt = _evt;
 
+           console.log(evt.keyCode);
+
+           this.deplacement(evt.keyCode);
        }
 
-       deplacement(dirCode){
+       deplacement(_dirCode){
+           switch(_dirCode){
+               case 37:
+                   this.nextMoveX = -1;
+                   this.nextMoveY = 0;
+                   break;
 
+               case 38:
+                   this.nextMoveX = 0;
+                   this.nextMoveY = -1;
+                   break;
+
+               case 39:
+                   this.nextMoveX = 1;
+                   this.nextMoveY = 0;
+                   break;
+
+               case 40:
+                   this.nextMoveX = 0;
+                   this.nextMoveY = 1;
+                   break;
+           }
+
+           //console.log(this.nextMoveX, this.nextMoveY);
        }
 
        controleSerpent(){
+           var nextX = this.currentX + this.nextMoveX;
+           var nextY = this.currentY + this.nextMoveY;
 
+           this.dessineCarre(nextX, nextY);
+           this.currentX = nextX;
+           this.currentY = nextY;
        }
 
        dessineCarre(x, y){
