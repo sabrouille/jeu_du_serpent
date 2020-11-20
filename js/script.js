e4fe5f61ae37c2fbb0ae0c9b335d6943a6d23f4b
Sarah Brouillet <sarah.brouillet@gmail.com>
2020-11-20 08:49:09 -0500

    Création des classes et constructor de base du jeu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
+    //Le jeu
+   class Jeu{   // Majuscule par convention
+       //Toujours commencer par une fct constructeur
+       constructor() {
+           console.log("Création du jeu");
+       }
+   }
 
+    //Le serpent
+    class Serpent{
+       constructor() {
+           console.log("Création du serpent");
+       }
+    }
+
+
+    //La pomme
+    class Pomme{
+       constructor() {
+           console.log("Création de la pomme");
+       }
+    }
+
+    var unePartie = new Jeu();
 
 });
